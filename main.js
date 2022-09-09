985cb4b76fcb2721ade5fbf65816bf77a7080bda
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-09-09 12:33:46 +0200

    Update
    added function "set_task"

--- main.js
+++ main.js
@@ -2,3 +2,10 @@
 let task_input;
 let task_due_date;
 
+
+function set_task(event){
+    event.preventDefault();
+    
+    task_input = document.getElementById("task-input").value;//fetching task input from html to assign value to it
+    task_due_date = document.getElementById("task-due-date").value;//fetching task-due-date input from html to assign value to it
+}
